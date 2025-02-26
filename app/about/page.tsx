69631f004e8ca9800d0bc87bee8ba5ff6ac8b7ff
imba97 <mail@imba97.cn>
2025-02-26 15:23:16 +0800

    feat: base router pages

---
+++ app/about/page.tsx
@@ -0,0 +1,5 @@
+export default function Properties() {
+  return (
+    <div className="font-itali text-center text-8">about page</div>
+  )
+}
---
+++ app/investment/page.tsx
@@ -0,0 +1,5 @@
+export default function Properties() {
+  return (
+    <div className="font-itali text-center text-8">investment page</div>
+  )
+}
