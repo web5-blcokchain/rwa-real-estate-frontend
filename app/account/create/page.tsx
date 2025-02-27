456ca73a05b59fd7d60f96bee948a909f3eb6b12
imba97 <mail@imba97.cn>
2025-02-27 10:05:16 +0800

    feat: return previous step on back button click

--- app/account/create/page.tsx
+++ app/account/create/page.tsx
@@ -2,17 +2,14 @@
 
 import CreateAccountSteps from '../-components/create-account-steps'
 import { AccountPanel } from '../-components/panels'
-import { StepsProvider } from '../-components/steps-provider'
 
 export default function AccountCreate() {
   return (
-    <StepsProvider>
-      <div className="h-full fccc gap-12">
-        <CreateAccountSteps />
-        <div className="w-full flex-1">
-          <AccountPanel />
-        </div>
+    <div className="h-full fccc gap-12">
+      <CreateAccountSteps />
+      <div className="w-full flex-1">
+        <AccountPanel />
       </div>
-    </StepsProvider>
+    </div>
   )
 }
--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,33 +1,41 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
+import { StepsProvider, useSteps } from './-components/steps-provider'
 
 export default function AccountLayout({
   children
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const router = useRouter()
-
   return (
-    <div className="size-full fccc px-8">
-      <div className="h-18 fbc w-full">
-        <button className="fyc select-none gap-1 text-[#898989] clickable" onClick={() => router.back()}>
-          <span className="i-material-symbols-light-arrow-back-rounded size-6"></span>
-          <span className="text-4">
-            Return
-          </span>
-        </button>
+    <StepsProvider>
+      <div className="size-full fccc px-8">
+        <div className="h-18 w-full fbc">
+          <ReturnButton />
 
-        <div className="fyc gap-1">
-          <div className="i-majesticons-globe-line size-5 bg-white"></div>
-          <div className="text-4">English</div>
-          <div className="i-ic-round-keyboard-arrow-down size-5 bg-white"></div>
+          <div className="fyc gap-1">
+            <div className="i-majesticons-globe-line size-5 bg-white"></div>
+            <div className="text-4">English</div>
+            <div className="i-ic-round-keyboard-arrow-down size-5 bg-white"></div>
+          </div>
+        </div>
+        <div className="w-full flex-1">
+          {children}
         </div>
       </div>
-      <div className="w-full flex-1">
-        {children}
-      </div>
-    </div>
+    </StepsProvider>
+  )
+}
+
+function ReturnButton() {
+  const { prev } = useSteps()
+
+  return (
+    <button className="fyc select-none gap-1 text-[#898989] clickable" onClick={prev}>
+      <span className="i-material-symbols-light-arrow-back-rounded size-6"></span>
+      <span className="text-4">
+        Return
+      </span>
+    </button>
   )
 }
