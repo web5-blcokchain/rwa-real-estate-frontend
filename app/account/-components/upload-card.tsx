dd503d9e0e50fead8cdafdd8db103e8a44febad2
imba97 <mail@imba97.cn>
2025-03-03 10:46:32 +0800

    feat: upload card icon class

--- app/account/-components/upload-card.tsx
+++ app/account/-components/upload-card.tsx
@@ -1,13 +1,17 @@
+import { cn } from '@/utils/style'
+
 const UploadCard: FC<{
   label: string
   title: string
   subTitle: string
   icon: string
+  iconClass?: string
 }> = ({
   label,
   title,
   subTitle,
   icon,
+  iconClass = '',
   children
 }) => {
   return (
@@ -16,7 +20,13 @@ const UploadCard: FC<{
 
       <div className="fccc select-none gap-4 b b-white rounded b-dashed py-6 clickable-99">
         <div>
-          <img src={icon} className="w-10" />
+          <img
+            src={icon}
+            className={cn(
+              'size-10',
+              iconClass
+            )}
+          />
         </div>
         <div>{title}</div>
         <div className="text-3 text-[#b5b5b5]">{subTitle}</div>
