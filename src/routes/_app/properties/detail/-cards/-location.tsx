2d28a29c3aa2d654ec9e012bce6ab0526dcc2217
imba97 <mail@imba97.cn>
2025-03-12 10:22:36 +0800

    feat: google map

--- src/routes/_app/properties/detail/-cards/-location.tsx
+++ src/routes/_app/properties/detail/-cards/-location.tsx
@@ -1,11 +1,31 @@
 import { DetailCard } from './-card'
 
 export const LocationCard: FC = () => {
+  const [loading, setLoading] = useState(true)
+
   return (
     <DetailCard title="Location">
       <div className="text-4 space-y-4">
-        <div className="h-32 fcc rounded-xl bg-background">
-          <div>TODO: Google map</div>
+        <div className="h-48 fcc of-hidden rounded-xl bg-background">
+          {
+            loading && (
+              <div>
+                <Waiting />
+              </div>
+            )
+          }
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3237.5745135709676!2d139.85366567516797!3d35.761261425574546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018857e4b9f72e7%3A0x208db05c46698b2d!2z44CSMTI1LTAwNTEgVG9reW8sIEthdHN1c2hpa2EgQ2l0eSwgTmlpanVrdSwgMi1jaMWNbWXiiJI14oiSOCDjg6zjgqrjg5Hjg6zjgrnjgqLjgqTjg6rjgrk!5e0!3m2!1szh-CN!2sjp!4v1741745705681!5m2!1szh-CN!2sjp"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            className={cn(
+              'b-none',
+              loading ? 'size-0' : 'size-full'
+            )}
+            onLoad={() => setLoading(false)}
+          >
+          </iframe>
         </div>
 
         <ul className="list-none [&>li]:(pl-4)">
