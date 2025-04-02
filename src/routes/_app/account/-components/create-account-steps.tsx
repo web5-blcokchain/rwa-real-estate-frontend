df2cbbd024af2235a5d5c4d146f11b1aa7a4f404
imba97 <mail@imba97.cn>
2025-04-02 18:17:35 +0800

    feat: create account steps back button

--- src/routes/_app/account/-components/create-account-steps.tsx
+++ src/routes/_app/account/-components/create-account-steps.tsx
@@ -2,7 +2,7 @@ import { cn } from '@/utils/style'
 import { CreateAccountStep, useSteps } from './steps-provider'
 
 export default function CreateAccountSteps() {
-  const { currentStep } = useSteps()
+  const { currentStep, prev } = useSteps()
 
   const steps = [
     {
@@ -20,22 +20,29 @@ export default function CreateAccountSteps() {
   ]
 
   return (
-    <div className="w-full fbc gap-1">
-      {
-        steps.map((item, i) => (
-          <div key={i} className="w-full">
-            <div className={cn(
-              'b b-text-secondary b-solid h-2',
-              currentStep === item.step ? 'bg-primary' : ''
-            )}
-            >
-            </div>
-            <div className="mt-4 text-center text-4">
-              {item.title}
-            </div>
-          </div>
-        ))
-      }
+    <div className="w-full">
+      <div className="w-full flex items-start gap-8">
+        <div className="fcc">
+          <div className="i-tabler-arrow-left size-8 bg-gray clickable" onClick={prev}></div>
+        </div>
+        <div className="fbc flex-1 gap-1">
+          {
+            steps.map((item, i) => (
+              <div key={i} className="w-full">
+                <div className={cn(
+                  'b b-text-secondary b-solid h-2',
+                  currentStep === item.step ? 'bg-primary' : ''
+                )}
+                >
+                </div>
+                <div className="mt-4 text-center text-4">
+                  {item.title}
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
