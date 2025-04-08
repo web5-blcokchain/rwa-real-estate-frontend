3bf6353faf4ccdf40927427e25bbaab4a2cdd42d
imba97 <mail@imba97.cn>
2025-04-08 15:26:14 +0800

    feat: historical income records

--- src/api/profile.ts
+++ src/api/profile.ts
@@ -3,3 +3,11 @@ import apiClient from './client'
 export function getOverView(data: Record<string, any>) {
   return apiClient.post('/api/info/overView', data)
 }
+
+export function getChartIncomeStatistics() {
+  return apiClient.get('/api/chart/income_statistics')
+}
+
+export function getChartIncomeTrend() {
+  return apiClient.get('/api/chart/income_trend')
+}
--- src/components/profile/history/historyEchart.tsx
+++ src/components/profile/history/historyEchart.tsx
@@ -1,49 +1,45 @@
+import { getChartIncomeTrend } from '@/api/profile'
+import { useQuery } from '@tanstack/react-query'
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 export const HistoryEchart: FC = () => {
-  const data = [
-    {
-      name: '09-01',
-      price: 0
-    },
-    {
-      name: '09-02',
-      price: 38
-    },
-    {
-      name: '09-03',
-      price: 0
-    },
-    {
-      name: '09-04',
-      price: 32
-    },
-    {
-      name: '09-05',
-      price: 36
-    },
-    {
-      name: '09-06',
-      price: 48
-    },
-    {
-      name: '09-07',
-      price: 100
+  const { data, isLoading } = useQuery({
+    queryKey: ['getCchartIncomeTrend'],
+    queryFn: async () => {
+      const res = await getChartIncomeTrend()
+      return res.data
     }
-  ]
+  })
+
+  console.log('data', data, isLoading)
+
+  if (isLoading) {
+    return (
+      <div className="h-32 fcc">
+        <Waiting iconClass="size-6" />
+      </div>
+    )
+  }
+
+  const chartData = data?.xAxis.map((date: string, index: number) => ({
+    date,
+    thisMonth: data.this_month[index]?.value || 0,
+    lastMonth: data.last_month[index]?.value || 0
+  })) || []
 
   return (
     <div className="h-32">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
-          data={data}
+          data={chartData}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <XAxis dataKey="date" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="linear" dataKey="price" stroke="#f0b90b" />
+          <Line type="linear" dataKey="thisMonth" stroke="#f0b90b" name="This Month" />
+          <Line type="linear" dataKey="lastMonth" stroke="#82ca9d" name="Last Month" />
         </LineChart>
       </ResponsiveContainer>
     </div>
