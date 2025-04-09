428ea2aa167a3f6b23cec09fd7a123de8d5719fe
imba97 <mail@imba97.cn>
2025-04-09 16:38:04 +0800

    feat: add useImagesPath utility function

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,3 +1,7 @@
 export function useImagePath(path: string) {
   return `${import.meta.env.VITE_PUBLIC_API_URL}${path}`
 }
+
+export function useImagesPath(paths: string) {
+  return paths?.split(',').map(useImagePath) || []
+}
