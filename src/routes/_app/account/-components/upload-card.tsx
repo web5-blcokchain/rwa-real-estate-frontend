419ecdde1d445096b6d7da4a095c9b72319e5782
imba97 <mail@imba97.cn>
2025-04-11 15:18:59 +0800

    feat: improve upload card

--- src/routes/_app/account/-components/upload-card.tsx
+++ src/routes/_app/account/-components/upload-card.tsx
@@ -1,3 +1,4 @@
+import { ImagePreview } from '@/components/common/image-preview'
 import { cn } from '@/utils/style'
 import { Upload } from 'antd'
 
@@ -8,6 +9,7 @@ const UploadCard: FC<{
   icon: string
   iconClass?: string
   beforeUpload?: (e: File) => void | undefined
+  src?: string | string[] // 添加图片地址属性
 }> = ({
   label,
   title,
@@ -15,11 +17,13 @@ const UploadCard: FC<{
   icon,
   iconClass = '',
   children,
-  beforeUpload
+  beforeUpload,
+  src
 }) => {
   return (
     <div className="rounded-xl bg-background-secondary p-6 space-y-4">
       <div className="text-4 text-white">{label}</div>
+
       <Upload
         className="UploadCard space-y-4"
         showUploadList={false}
@@ -29,24 +33,39 @@ const UploadCard: FC<{
         }}
         disabled={!beforeUpload}
       >
-        <div className={cn(
-          'fccc select-none gap-4 b b-white rounded b-dashed py-6',
-          beforeUpload ? 'clickable-99' : ''
-        )}
-        >
-          <div>
-            <img
-              src={icon}
-              className={cn(
-                'size-10',
-                iconClass
-              )}
-            />
-          </div>
-          <div className="text-white">{title}</div>
-          <div className="text-3 text-[#b5b5b5]">{subTitle}</div>
-        </div>
+        {
+          src && src.length > 0
+            ? (
+                <div className="space-y-4">
+                  <ImagePreview
+                    src={src}
+                    imageContainerClass="rounded-lg of-hidden"
+                  />
+                </div>
+              )
+            : (
+                <div className={cn(
+                  'fccc select-none gap-4 b b-white rounded b-dashed py-6',
+                  beforeUpload ? 'clickable-99' : ''
+                )}
+                >
+                  <div>
+                    <img
+                      src={icon}
+                      className={cn(
+                        'size-10',
+                        iconClass
+                      )}
+                    />
+                  </div>
+                  <div className="text-white">{title}</div>
+                  <div className="text-3 text-[#b5b5b5]">{subTitle}</div>
+                </div>
+              )
+        }
+
       </Upload>
+
       {children}
     </div>
   )
