2c382f8f49370bc039202061c4bd1d6a285af080
imba97 <mail@imba97.cn>
2025-04-14 10:44:22 +0800

    feat: use antd dark theme

--- src/components/provider/antd.tsx
+++ src/components/provider/antd.tsx
@@ -1,11 +1,14 @@
-import { ConfigProvider } from 'antd'
+import { ConfigProvider, theme } from 'antd'
+
+const { darkAlgorithm } = theme
 
 export const AntdProvider: FC = ({ children }) => {
   return (
     <ConfigProvider theme={{
       token: {
         colorPrimary: '#f5d930'
-      }
+      },
+      algorithm: [darkAlgorithm]
     }}
     >
       {children}
