06ece515dd72ff2e5161940b32aab15d18d2e88b
imba97 <mail@imba97.cn>
2025-04-17 14:08:40 +0800

    feat: add global env

--- src/lib/global.ts
+++ src/lib/global.ts
@@ -2,7 +2,11 @@ import ky from 'ky'
 
 export const Env = {
   apiUrl: import.meta.env.VITE_PUBLIC_API_URL,
-  privyAppId: import.meta.env.VITE_PUBLIC_PRIVY_APP_ID
+  privyAppId: import.meta.env.VITE_PUBLIC_PRIVY_APP_ID,
+  web3: {
+    rpc: import.meta.env.VITE_PUBLIC_WEB3_RPC_URL,
+    chainId: import.meta.env.VITE_PUBLIC_WEB3_CHAIN_ID
+  }
 }
 
 export const Global = {
