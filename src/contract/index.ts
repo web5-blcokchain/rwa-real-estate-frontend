e5dfc0cb1637d50ba05d3765cb5f312a14e2aa98
imba97 <mail@imba97.cn>
2025-04-21 17:07:44 +0800

    feat: add PropertyManager contract

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,4 +1,5 @@
 import { getWeb3Instance } from '@/utils/web3'
+import PropertyManager from './PropertyManager.json'
 import SimpleERC20 from './SimpleERC20.json'
 import TradingManager from './TradingManager.json'
 
@@ -7,6 +8,16 @@ export function useTradingManagerContract() {
   const contract = new web3.eth.Contract(
     TradingManager.abi,
     '0x905Ad472d7eeB94ed1Fc29D8ff4B53FD4D5a5Eb4'
+  )
+
+  return contract
+}
+
+export function usePropertyManagerContract() {
+  const web3 = getWeb3Instance()
+  const contract = new web3.eth.Contract(
+    PropertyManager.abi,
+    '0x2f6f107D4Afd43c451B74DA41A6DDA53D2Bf24B1'
   )
 
   return contract
