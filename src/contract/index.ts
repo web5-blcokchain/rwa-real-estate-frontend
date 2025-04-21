3592d7d3c5f838408df8a3c1fd89aa0e1c230be0
imba97 <mail@imba97.cn>
2025-04-21 16:14:20 +0800

    feat: update contract scripts

--- src/contract/index.ts
+++ src/contract/index.ts
@@ -1,13 +1,22 @@
-import { Env } from '@/lib/global'
-import Web3 from 'web3'
-
+import { getWeb3Instance } from '@/utils/web3'
+import SimpleERC20 from './SimpleERC20.json'
 import TradingManager from './TradingManager.json'
 
 export function useTradingManagerContract() {
-  const web3 = new Web3(Env.web3.rpc)
+  const web3 = getWeb3Instance()
   const contract = new web3.eth.Contract(
     TradingManager.abi,
     '0x905Ad472d7eeB94ed1Fc29D8ff4B53FD4D5a5Eb4'
+  )
+
+  return contract
+}
+
+export function useSimpleERC20Contract() {
+  const web3 = getWeb3Instance()
+  const contract = new web3.eth.Contract(
+    SimpleERC20.abi,
+    '0xeF66010868Ff77119171628B7eFa0F6179779375'
   )
 
   return contract
