4f105d27eea6886337c02e9fcd38fbd10f724fe4
imba97 <mail@imba97.cn>
2025-04-26 20:05:04 +0800

    feat: update api type

--- src/api/investment.ts
+++ src/api/investment.ts
@@ -8,14 +8,14 @@ export function getInvestmentList(data: {
 }
 
 export function buyAsset(data: {
-  order_market_id: number
+  order_id: number
 }) {
   return apiClient.post('/api/market/confirmBuy', data)
 }
 
 export function sellAsset(data: {
-  order_market_id: number
-  hash: string
+  id: number
+  sell_order_id: number
 }) {
   return apiClient.post('/api/market/confirmSell', data)
 }
