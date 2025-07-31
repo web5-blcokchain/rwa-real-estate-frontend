c6f627cea496543c4cf020a855d77d93952365f9
1355600640 <1355600640@qq.com>
2025-07-31 21:41:05 +0800

    fix(components, routes): Enhance user logout functionality by adding clearRegisterData call and implement user registration data handling in the account creation route

--- src/routes/_app/account/create/index.lazy.tsx
+++ src/routes/_app/account/create/index.lazy.tsx
@@ -1,5 +1,8 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { useUserStore } from '@/stores/user'
 
+import { getToken } from '@/utils/user'
+import { usePrivy } from '@privy-io/react-auth'
+import { createLazyFileRoute, useNavigate } from '@tanstack/react-router'
 import VerificationPanel from '../-components/panels/verification'
 
 export const Route = createLazyFileRoute('/_app/account/create/')({
@@ -7,6 +10,37 @@ export const Route = createLazyFileRoute('/_app/account/create/')({
 })
 
 function RouteComponent() {
+  const { user, ready, authenticated } = usePrivy()
+  const { setRegisterData, userData } = useUserStore()
+  const navigate = useNavigate()
+
+  const token = getToken()
+
+  useEffect(() => {
+    if (
+      !ready
+      || !authenticated
+      || !user?.wallet
+      || !token
+    ) {
+      return
+    }
+
+    setRegisterData({
+      token: token!,
+      email: user.email?.address || user.google?.email
+    })
+  }, [user?.email?.address, user?.google?.email, token])
+
+  useEffect(() => {
+    // 1的时候表示已审核 2:中心化数据库审核拒绝 3.钱包绑定成功 4.kyc审核通过 5 kyc审核拒绝
+    if (userData.audit_status && ![2, 5].includes(userData.audit_status)) {
+      navigate({
+        to: '/home'
+      })
+    }
+  }, [userData])
+
   return (
     // <StepsProvider>
     //   <div className="fccc gap-12">
